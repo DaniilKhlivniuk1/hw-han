e5d557f94f46578d29863f5d054919daa309391a
DaniilKhlivniuk1 <141495344+DaniilKhlivniuk1@users.noreply.github.com>
2025-04-03 22:03:21 +0300

    Updates

--- hw-han.579125c3.js
+++ hw-han.579125c3.js
@@ -1,32 +1,39 @@
-// Завдання 1: 
-document.getElementById("startHourButton").addEventListener("click", startHourTimer);
-function startHourTimer() {
-    let timeLeft = 60;
-    let interval = setInterval(()=>{
-        document.getElementById("hourTimerMessage").textContent = `\u{417}\u{430}\u{43B}\u{438}\u{448}\u{438}\u{43B}\u{43E}\u{441}\u{44C}: ${timeLeft} \u{445}\u{432}\u{438}\u{43B}\u{438}\u{43D}`;
-        if (timeLeft === 30) alert("\u0417\u0430\u043B\u0438\u0448\u0438\u043B\u043E\u0441\u044C \u043C\u0435\u043D\u0448\u0435 \u043F\u043E\u043B\u043E\u0432\u0438\u043D\u0438 \u0447\u0430\u0441\u0443!");
-        if (timeLeft === 0) clearInterval(interval);
-        timeLeft--;
-    }, 60000);
-}
-// Завдання 2: 
-const startButton = document.getElementById("startButton");
-startButton.addEventListener("click", startSecondTimer);
-function startSecondTimer() {
-    let timeLeft = 30000;
-    startButton.disabled = true;
-    let interval = setInterval(()=>{
-        document.getElementById("secondTimerMessage").textContent = `\u{417}\u{430}\u{43B}\u{438}\u{448}\u{438}\u{43B}\u{43E}\u{441}\u{44C}: ${(timeLeft / 1000).toFixed(1)} \u{441}\u{435}\u{43A}\u{443}\u{43D}\u{434}`;
-        if (timeLeft <= 10000) animateEffect();
-        if (timeLeft <= 0) {
-            clearInterval(interval);
-            startButton.disabled = false;
+class CountdownTimer {
+    constructor({ selector, targetDate }){
+        this.selector = selector;
+        this.targetDate = targetDate;
+        this.timerElement = document.querySelector(this.selector);
+        this.refs = {
+            days: this.timerElement.querySelector('[data-value="days"]'),
+            hours: this.timerElement.querySelector('[data-value="hours"]'),
+            mins: this.timerElement.querySelector('[data-value="mins"]'),
+            secs: this.timerElement.querySelector('[data-value="secs"]')
+        };
+        this.start();
+    }
+    start() {
+        this.updateTimer();
+        this.intervalId = setInterval(()=>this.updateTimer(), 1000);
+    }
+    updateTimer() {
+        const time = this.targetDate - new Date();
+        if (time < 0) {
+            clearInterval(this.intervalId);
+            return;
         }
-        timeLeft -= 10;
-    }, 10);
+        const days = Math.floor(time / 86400000);
+        const hours = Math.floor(time % 86400000 / 3600000);
+        const mins = Math.floor(time % 3600000 / 60000);
+        const secs = Math.floor(time % 60000 / 1000);
+        this.refs.days.textContent = days;
+        this.refs.hours.textContent = String(hours).padStart(2, '0');
+        this.refs.mins.textContent = String(mins).padStart(2, '0');
+        this.refs.secs.textContent = String(secs).padStart(2, '0');
+    }
 }
-function animateEffect() {
-    document.body.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
-}
+new CountdownTimer({
+    selector: '#timer-1',
+    targetDate: new Date('Jul 17, 2025')
+});
 
 //# sourceMappingURL=hw-han.579125c3.js.map
